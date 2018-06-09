797d88993f686e9ed275292d92568f18ef041827
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2018-06-08 21:27:55 -0700

    Bubble Sort Algorithm

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -1,4 +1,5 @@
 //Bubble Sort
+#include<cstdlib>
 #include<iostream>
 
 using namespace std;
@@ -12,14 +13,15 @@ private:
 
 int main(){
   BubbleSort A;
-  const int SIZE_OF_ARRAY = 5;
-  int y[SIZE_OF_ARRAY]={4,2,5,3,6};
-  cout<<"Before sorting y[SIZE_OF_ARRAY] = ";
-  for (size_t i = 0; i < SIZE_OF_ARRAY; i++) {
-    cout<<y[i]<<" ";
+  int y[25];
+  for (size_t i = 0; i < 25; i++){
+    y[i]= rand();
   }
-  cout<<endl;
-  A.Array(y,SIZE_OF_ARRAY);
+  cout<<"\nBefore Sorting y[] = ";
+  for (size_t i = 0; i < 25; i++) {
+    cout<<y[i]<<", ";
+  }cout<<endl;
+  A.Array(y,25);
 //Another way of using the for loop
 // for (int x: y) {
 //  cout<<x<<endl;
@@ -28,18 +30,22 @@ return 0;
 }
 
 void BubbleSort::Array(int a[],int size){
-  for(int i = 0;i<size;i++){
-  if (a[i]>a[i+1] && a[i+1] != size)
-  Swap(a[i],a[i+1]);
- }
- for(int i = 0;i<size;i++){
- if (a[i]>a[i+1] && a[i+1] != size)
- Swap(a[i],a[i+1]);
-}cout<<"After sorting y[SIZE_OF_ARRAY] = ";
+  int j = 1;
+  while(j != 0){
+    j = 0;
+    for(int i = 0;i<size;i++){
+    if (a[i]>a[i+1] && a[i+1] != size){
+    Swap(a[i],a[i+1]);
+    j=j+1;}
+  }
+}
+cout<<endl;
+cout<<"After Sorting y[] = ";
 for (size_t i = 0; i < size; i++) {
-  cout<<a[i]<<" ";
+  cout<<a[i]<<", ";
+}cout<<endl;
 }
-}
+
 void BubbleSort::Swap(int& a,int& b){
   int temp;
   temp = a;
---
+++ Merger_Sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main(){
+int Array[60];
+for (size_t i = 0; i < 60; i++) {
+   Array[i]=i*13%4;
+}
+
+  return 0;
+}
